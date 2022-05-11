dd8a122dd12eefd39f2df218c0aa216a2184ba13
AhmedSamir1271 <egahmedsamir1271@gmail.com>
2022-05-11 13:42:29 +0200

    Add project files.

---
+++ WebApplication7SE2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication7SE2.MvcApplication" Language="C#" %>
---
+++ WebApplication7SE2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication7SE2.Startup))]
+namespace WebApplication7SE2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
